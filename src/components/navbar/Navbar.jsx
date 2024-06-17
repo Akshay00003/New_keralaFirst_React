72d060d48588e4ed9eac7d7953638a6e7240cda2
Akshay00003 <akshayprakash4003@gmail.com>
2024-06-18 00:09:04 +0530

    new

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import  { useState } from "react";
 import style from "./Navbar.module.scss";
-import { IoIosArrowDown, IoMdCart } from "react-icons/io";
+// import { IoIosArrowDown, IoMdCart } from "react-icons/io";
 import { IoSearch } from "react-icons/io5";
 import logo from "../../assets/images/logo.png";
 import { TiThMenu } from "react-icons/ti";
@@ -16,27 +16,27 @@ const Navbar = () => {
           <ul>
             <li>
               <a href="">Home</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
             <li>
               <a href="">Courses</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
             <li>
               <a href="">Pages</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
             <li>
               <a href="">Blog</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
             <li>
               <a href="">Shop</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
             <li>
               <a href="">Contact</a>
-              <IoIosArrowDown className={style.icon} />
+              {/* <IoIosArrowDown className={style.icon} /> */}
             </li>
           </ul>
         </div>}
@@ -44,7 +44,7 @@ const Navbar = () => {
       </div>
       <div className={style.right}>
         <IoSearch size={25} />
-        <IoMdCart size={25} />
+        {/* <IoMdCart size={25} /> */}
         <TiThMenu size={25} onClick={()=>setMenu(!menu)} className={style.menu} />
       </div>
     </div>
